f47228139090181a3d1a720408babb447306ae03
NanoSymmetry <NanoSymmetry@users.noreply.github.com>
2022-01-29 00:19:15 -0500

    Add sanitizing of inputs

--- crossCompile.sh
+++ crossCompile.sh
@@ -8,6 +8,9 @@ if [[ ! -d "src" ]]; then mkdir src; fi
 
 grep -v '^#' arch.txt | while read -r line; do
 
+    # Sanitize input by only allowing alpha-numeric charachters
+    line=${line//[^a-zA-Z0-9_]/}
+
     #Make sure it doesnt exist before (re)making it
     if [[ ! -f crossCompilers/dockcross-$line ]]; then
         docker run --rm dockcross/$(echo $line) > crossCompilers/dockcross-$(echo $line)
