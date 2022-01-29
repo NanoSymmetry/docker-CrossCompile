fe730cbc7ca0bd5b73e3be42bf598899f901c136
NanoSymmetry <NanoSymmetry@users.noreply.github.com>
2022-01-28 23:28:31 -0500

    Made the script work

--- crossCompile.sh
+++ crossCompile.sh
@@ -8,14 +8,13 @@ if [[ ! -d "src" ]]; then mkdir src; fi
 
 grep -v '^#' arch.txt | while read -r line; do
 
-    # Make sure it doesnt exist before (re)making it
+    #Make sure it doesnt exist before (re)making it
     if [[ ! -f crossCompilers/dockcross-$line ]]; then
-        #echo "It doesnt exist"
-        docker run --rm dockcross/'$(echo $line)' > "crossCompilers/dockcross-''$(echo $line)'"
+        docker run --rm dockcross/$(echo $line) > crossCompilers/dockcross-$(echo $line)
         chmod +x crossCompilers/dockcross-$line
     fi
 
     # Actually compile the shit
-    ./dockcross-$line bash -c '$CXX ./src/main.cpp ./src/cubiomes/*.c -O2 -static -static-libgcc -static-libstdc++ -lpthread -lm -o $line'
+    ./crossCompilers/dockcross-$(echo $line) bash -c '$CXX src/main.cpp src/cubiomes/*.c -O2 -static -static-libgcc -static-libstdc++ -lpthread -lm'
 
 done
